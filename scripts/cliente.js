25813e66fa199115599ab0e52446668f77cf981c
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-28 13:17:44 +0000

    added css for client

--- scripts/cliente.js
+++ scripts/cliente.js
@@ -1,3 +1,9 @@
 function setActive(id){
+    const activeItem = document.querySelector(".active")
+    activeItem.classList.remove("active") 
+    activeItem.classList.remove("disabled") 
     
+    const activateItem = document.getElementById(id)
+    activateItem.classList.remove("disabled")
+    activateItem.classList.active("active")
 }

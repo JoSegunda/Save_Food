91050cca4ab930ce930522d3e20b10f2ee166d1e
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-28 09:52:24 +0000

    updated  js for restaurante

--- scripts/restaurante.js
+++ scripts/restaurante.js
@@ -60,40 +60,23 @@ document.addEventListener("DOMContentLoaded", () =>{
         return box;
     }
     // Mostrar Feedback
+    function showFeedback(type, msg) {
+        feedback.style.display = "block";
+        feedback.innerText = msg;
+
+        if (type === "sucesso") {
+            feedback.style.background = "#d4f8d4";
+            feedback.style.color = "#1d572e";
+            feedback.style.border = "2px solid #2b8f3e";
+        } else {
+            feedback.style.background = "#f9d6d6";
+            feedback.style.color = "#8f1f1f";
+            feedback.style.border = "2px solid #b82d2d";
+        }
+    }
+
 
 
 })
 
 
-
-// CARREGAR OFERTAS ATIVAS
-async function loadOffers(){
-    offersContainer.innerHtml = "<h1>Suas ofertas ativas</h1> <p>A carregar ....</p>"
-
-    try {
-        const resp = await fetch("https://magno.di.uevora.pt/tweb/t1/oferta/list")
-        const ofertas = await resp.json();
-
-        if(!Array.isArray(ofertas)){
-            offersContainer.innerHtml = "<p>Nenhuma Oferta Encontrada.</p>"
-            return
-        }
-        // Contéudo dentro do card de ofertas
-        let html = "<h1>Suas ofertas ativas</h1>"
-        ofertas.forEach(o =>{
-            html += `
-            <div class="oferta-card">
-                    <div class="oferta-card-img" style="background-image: url('${o.imagem || "https://via.placeholder.com/150"}')"></div>
-                    <div class="oferta-card-info">
-                        <h4>${o.titulo}</h4>
-                        <p><strong>Descrição:</strong> ${o.descricao}</p>
-                        <p><strong>Quantidade:</strong> ${o.quantidade}</p>
-                        <p><strong>ID:</strong> ${o.id}</p>
-                    </div>
-                </div>`;
-        });
-        offersContainer.innerHtml = html
-    } catch (error) {
-        offersContainer.innerHtml="<p>Erro ao carregar ofertas.</p>"
-    }
-}

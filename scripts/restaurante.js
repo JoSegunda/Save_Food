899188f93b6120df7de399b3946fb9aeed3c963a
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-28 09:34:06 +0000

    updated  js for restaurante

--- scripts/restaurante.js
+++ scripts/restaurante.js
@@ -9,6 +9,19 @@ document.addEventListener("DOMContentLoaded", () =>{
     loadOffers()
 })
 
-function loadOffers(){
+// CARREGAR OFERTAS ATIVAS
+async function loadOffers(){
     offersContainer.innerHtml = "<h1>Suas ofertas ativas</h1> <p>A carregar ....</p>"
+
+    try {
+        const resp = await fetch("https://magno.di.uevora.pt/tweb/t1/oferta/list")
+        const ofertas = await resp.json();
+
+        if(!Array.isArray(ofertas)){
+            offersContainer.innerHtml = "<p>Nenhuma Oferta Encontrada.</p>"
+            return
+        }
+    } catch (error) {
+        
+    }
 }

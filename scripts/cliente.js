bd301e32bc6e8cb65c3717fab7e3e1a1effb6f34
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-28 13:42:58 +0000

    added js for client

--- scripts/cliente.js
+++ scripts/cliente.js
@@ -4,14 +4,18 @@ function setActive(id){
     previousItem.classList.remove("active")
     previousItem.classList.add("disabled")
 
-    const currentlyDisplaying = document.getElementsByClassName("show")
-    currentlyDisplaying.classList.remove("show")
-    currentlyDisplaying.classList.add("no-show")
-
     // New Item
     const newActiveItem = document.getElementById(id)
     newActiveItem.classList.remove("disabled")
     newActiveItem.classList.add("active")
 
-    const newDisplay = document.getElementById("show"+id)
+    // remove current description
+    const currentlyDisplaying = document.getElementsByClassName("show")[0]
+    currentlyDisplaying.classList.remove("show")
+    currentlyDisplaying.classList.add("no-show")
+    // Adding new description
+    var show = "show-"+id
+    const newDisplay = document.getElementById(show)
+    newDisplay.classList.remove("no-show")
+    newDisplay.classList.add("show")
 }

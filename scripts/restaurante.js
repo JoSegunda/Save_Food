abe69cefbe573defd7c9c2b85b3d3b70f5908d67
Joaquim Segunda <joelpereirasegunda@gmail.com>
2025-11-28 09:29:27 +0000

    updated  js for restaurante

---
+++ scripts/restaurante.js
@@ -0,0 +1,11 @@
+console.log("Working")
+
+document.addEventListener("DOMContentLoaded", () =>{
+
+    const form = document.querySelector("#ofertas")
+    const feedback = feedbackBox();
+    const offersContainer = document.querySelector("#Ofertas disponíveis")
+
+    loadOffers()
+})
+
